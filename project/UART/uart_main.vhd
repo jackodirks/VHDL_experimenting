521a623862ab9d95209e1fe123696e493b9b00c9
Jacko Dirks <jacko.dirks@gmail.com>
2016-02-27 21:32:14 +0100

    Moved parts of uart_main to uart_receiv, readded different baud for in and out

--- project/UART/uart_main.vhd
+++ project/UART/uart_main.vhd
@@ -1,8 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Baudrate for incoming and outgoing is the same.
-
 -- A note about parity:
 -- 0: odd parity
 -- 1: even parity
@@ -12,7 +10,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity uart_main is
     generic (
-        baudrate                : integer;
+        send_baudrate           : natural;
+        receive_baudrate        : natural;
         pary_bit_in             : boolean;
         parity_bit_in_type      : integer range 0 to 3;
         bit_count_in            : integer range 5 to 9;
@@ -24,6 +23,8 @@ entity uart_main is
     );
     Port (
         rst                     : in STD_LOGIC;
+        snd_rst                 : in STD_LOGIC;
+        rcv_rst                 : in STD_LOGIC;
         clk                     : in STD_LOGIC;
         uart_rx                 : in STD_LOGIC;
         uart_tx                 : out STD_LOGIC;
@@ -37,26 +38,12 @@ entity uart_main is
 end uart_main;
 
 architecture Behavioral of uart_main is
-    component simple_multishot_timer is
-        generic (
-            match_val : integer
-        );
-        port (
-            clk_50Mhz   : in STD_LOGIC;
-            rst         : in STD_LOGIC;
-            done        : out STD_LOGIC
-        );
-    end component;
 
-    constant clockspeed     : integer := 5E7;
-    constant oversampling   : integer := 8;
+    constant clockspeed     : natural := 5E7;
     constant sendSpeed      : integer := integer(clockspeed/baudrate);
-    constant receiveSpeed   : integer := integer(clockspeed/(baudrate*8));
 
     signal send_ticker_rst  : STD_LOGIC := 1;
     signal send_ticker_done : STD_LOGIC;
-    signal recv_ticker_rst  : STD_LOGIC := 1;
-    signal recv_ticker_done : STD_LOGIC;
 
 begin
     -- Create two clocks. The first one is for receiving and is oversampling * baudrate.
@@ -71,13 +58,4 @@ begin
         done        => send_ticker_done
     );
 
-    receive_ticker : simple_multishot_timer
-    generic map (
-        match_val   => receiveSpeed
-    )
-    port map (
-        clk_50Mhz   => clk,
-        rst         => recv_ticker_rst,
-        done        => recv_ticker_done
-    );
 end Behavioral;
