32c8a009e41df5c647ee20943559ed105f3f6aaf
Jacko Dirks <jacko.dirks@gmail.com>
2016-02-28 12:15:11 +0100

    Reformatted seven segment parts

--- project/sevenSegment/bits_to_seven_segement_translation.vhd
+++ project/sevenSegment/bits_to_seven_segement_translation.vhd
@@ -2,29 +2,31 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity bits_to_seven_segement_translation is
-    Port ( bit_input : in  STD_LOGIC_VECTOR (3 downto 0);
-           ss_out : out  STD_LOGIC_VECTOR (7 downto 0));
+    Port (
+        bit_input : in  STD_LOGIC_VECTOR (3 downto 0);
+        ss_out : out  STD_LOGIC_VECTOR (7 downto 0)
+    );
 end bits_to_seven_segement_translation;
 
 architecture Behavioral of bits_to_seven_segement_translation is
 
 begin
- ss_out <=  "11000000" when (bit_input = "0000") else
-                    "11111001" WHEN (bit_input = "0001") else
-                    "10100100" WHEN (bit_input = "0010") else
-                    "10110000" WHEN (bit_input = "0011") else
-                    "10011001" WHEN (bit_input = "0100") else
-                    "10010010" WHEN (bit_input = "0101") else
-                    "10000010" WHEN (bit_input = "0110") else
-                    "11111000" WHEN (bit_input = "0111") else
-                    "10000000" WHEN (bit_input = "1000") else
-                    "10010000" WHEN (bit_input = "1001") else
-                    "10001000" WHEN (bit_input = "1010") else
-                    "10000011" WHEN (bit_input = "1011") else
-                    "11000110" WHEN (bit_input = "1100") else
-                    "10100001" WHEN (bit_input = "1101") else
-                    "10000110" WHEN (bit_input = "1110") else
-                    "10001110" WHEN (bit_input = "1111") else
-                    "00000000";
+    ss_out <=  "11000000" when (bit_input = "0000") else
+               "11111001" when (bit_input = "0001") else
+               "10100100" when (bit_input = "0010") else
+               "10110000" when (bit_input = "0011") else
+               "10011001" when (bit_input = "0100") else
+               "10010010" when (bit_input = "0101") else
+               "10000010" when (bit_input = "0110") else
+               "11111000" when (bit_input = "0111") else
+               "10000000" when (bit_input = "1000") else
+               "10010000" when (bit_input = "1001") else
+               "10001000" when (bit_input = "1010") else
+               "10000011" when (bit_input = "1011") else
+               "11000110" when (bit_input = "1100") else
+               "10100001" when (bit_input = "1101") else
+               "10000110" when (bit_input = "1110") else
+               "10001110" when (bit_input = "1111") else
+               "00000000";
 end Behavioral;
 
--- project/sevenSegment/seven_segements_driver.vhd
+++ project/sevenSegment/seven_segements_driver.vhd
@@ -2,54 +2,62 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity seven_segments_driver is
-    generic ( switch_freq : integer );
+    generic (
+        switch_freq         : integer
+    );
     Port (
-        clk_50Mhz           : in  STD_LOGIC;
-        ss_1                : in  STD_LOGIC_VECTOR (3 downto 0);
-        ss_2                : in  STD_LOGIC_VECTOR (3 downto 0);
-        ss_3                : in  STD_LOGIC_VECTOR (3 downto 0);
-        ss_4                : in  STD_LOGIC_VECTOR (3 downto 0);
-        seven_seg_kath      : out  STD_LOGIC_VECTOR (7 downto 0);
-        seven_seg_an        : out  STD_LOGIC_VECTOR (3 downto 0)
+        clk_50Mhz           : in    STD_LOGIC;
+        ss_1                : in    STD_LOGIC_VECTOR (3 downto 0);
+        ss_2                : in    STD_LOGIC_VECTOR (3 downto 0);
+        ss_3                : in    STD_LOGIC_VECTOR (3 downto 0);
+        ss_4                : in    STD_LOGIC_VECTOR (3 downto 0);
+        seven_seg_kath      : out   STD_LOGIC_VECTOR (7 downto 0);
+        seven_seg_an        : out   STD_LOGIC_VECTOR (3 downto 0)
     );
 end seven_segments_driver;
 
 architecture Behavioral of seven_segments_driver is
+
     component bits_to_seven_segement_translation
         Port (
-            bit_input : in  STD_LOGIC_VECTOR (3 downto 0);
-            ss_out : out  STD_LOGIC_VECTOR (7 downto 0)
+            bit_input       : in  STD_LOGIC_VECTOR (3 downto 0);
+            ss_out          : out  STD_LOGIC_VECTOR (7 downto 0)
         );
     end component;
+
     component simple_multishot_timer is
-        generic ( match_val   : integer );
+        generic (
+            match_val       : integer
+        );
         port (
-            clk_50Mhz   : in STD_LOGIC;
-            rst         : in STD_LOGIC;
-            done        : out STD_LOGIC
+            clk_50Mhz       : in    STD_LOGIC;
+            rst             : in    STD_LOGIC;
+            done            : out   STD_LOGIC
         );
     end component;
+
     type state_type is (first, second, third, fourth);
-    signal state : state_type := first;
-    signal simple_multishot_timer_done : STD_LOGIC;
-    signal ss_curVal_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+    signal state                        : state_type := first;
+    signal simple_multishot_timer_done  : STD_LOGIC;
+    signal ss_curVal_out                : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
 begin
 
     simple_multishot_timer_wait_time : simple_multishot_timer
     generic map (
-        match_val => 50000000 / switch_freq
+        match_val   => 50000000 / switch_freq
     )
     port map (
-        clk_50MHZ => clk_50Mhz,
-        rst => '0',
-        done => simple_multishot_timer_done
+        clk_50MHZ   => clk_50Mhz,
+        rst         => '0',
+        done        => simple_multishot_timer_done
     );
 
     translator : bits_to_seven_segement_translation
     port map (
-        bit_input => ss_curVal_out,
-        ss_out => seven_seg_kath
+        bit_input   => ss_curVal_out,
+        ss_out      => seven_seg_kath
     );
     process (clk_50Mhz, ss_1, ss_2, ss_3, ss_4)
     begin
