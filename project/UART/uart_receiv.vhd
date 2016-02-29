2184d3629b37e1a11b752038ba28d921eb188a9b
Jacko Dirks <jacko.dirks@gmail.com>
2016-02-29 15:33:34 +0100

    Seemingly finished state transitions for UART receiver

--- project/UART/uart_receiv.vhd
+++ project/UART/uart_receiv.vhd
@@ -20,13 +20,13 @@ entity uart_receiv is
         stop_bits_in            : Natural range 1 to 2
     );
     port (
-        rst                     : in STD_LOGIC;
-        clk                     : in STD_LOGIC;
-        uart_rx                 : in STD_LOGIC;
-        receved_data            : out STD_LOGIC_VECTOR(8 DOWNTO 0);
-        data_ready              : out STD_LOGIC;                    -- Signals that data has been received.
-        parity_error            : out STD_LOGIC;                    -- Signals that the parity check has failed, is zero if there was none
-        data_error              : out STD_LOGIC                     -- Signals that data receiving has encoutered errors
+        rst                     : in    STD_LOGIC;
+        clk                     : in    STD_LOGIC;
+        uart_rx                 : in    STD_LOGIC;
+        receved_data            : out   STD_LOGIC_VECTOR(8 DOWNTO 0);
+        data_ready              : out   STD_LOGIC;                    -- Signals that data has been received.
+        parity_error            : out   STD_LOGIC;                    -- Signals that the parity check has failed, is zero if there was none
+        data_error              : out   STD_LOGIC                     -- Signals that data receiving has encoutered errors
     );
 end uart_receiv;
 
@@ -42,8 +42,11 @@ architecture Behavioral of uart_receiv is
         );
     end component;
 
-    type state_type is (rst, wait_start, start_start, start_bit_one, start_bit_two, start_end,
-    bit_start, bit_read_one, bit_read_two, bit_end, parity_start, parity_read_one, parity_read_two, parity_end, stop_start, stop_read_one, stop_read_two, stop_end);
+    type state_type is (rst, wait_start,
+    start_start, start_bit_one, start_bit_two, start_end,
+    bit_start, bit_read_one, bit_read_two, bit_end,
+    parity_start, parity_read_one, parity_read_two, parity_end,
+    stop_start, stop_read_one, stop_read_two, stop_end);
 
     constant oversampling   : Natural := 4;
     constant receiveSpeed   : integer := integer(clockspeed/(baudrate*oversampling));
@@ -51,6 +54,15 @@ architecture Behavioral of uart_receiv is
     signal recv_ticker_rst  : STD_LOGIC := 1;
     signal recv_ticker_done : STD_LOGIC;
     signal state            : state_type := rst;
+
+    function simple_state_transition(if0: state_type, if1 : state_type, var: STD_LOGIC) return state_type is
+    begin
+        if var = '1' then
+            return if1;
+        else
+            return if0;
+        end if;
+    end simple_state_transition;
 
 begin
 
@@ -67,54 +79,36 @@ begin
     -- State transitions
     process(clk, rst, uart_rx)
         -- State transition control variables
-        variable ticks_passed   : natural := 0;
-        variable bits_processed : natural := 0;
+        variable bits_processed         : natural := 0;
+        variable stop_bits_processed    : natural := 0;
     begin
         if rst = '1' then
             state <= rst;
         elsif rising_edge(clk) then
             case state is
+-- rst, wait_start,
                 when rst =>
-                    ticks_passed := 0;
+                    bits_processed := 0;
                     state <= wait_start;
                 when wait_start =>
-                    ticks_passed := 0;
-                    if uart_rx = '0' then
-                        state <= process_start;
-                    else
-                        state <= wait_start;
-                    end if;
-                when process_start =>
                     bits_processed := 0;
-                    if recv_ticker_done = '1' then
-                        ticks_passed := ticks_passed + 1;
-                        if ticks_passed = oversampling then
-                            state <= bit_start;
-                        else
-                            state <= process_start;
-                        end if;
-                    else
-                        state <= process_start;
-                    end if;
+                    state <= simple_state_transition(start_start, wait_start, uart_rx);
+                when start_start =>
+-- start_start, start_bit_one, start_bit_two, start_end,
+                    state <= simple_state_transition(start_start, start_bit_one, recv_ticker_done);
+                when start_bit_one =>
+                    state <= simple_state_transition(start_bit_one, start_bit_two, recv_ticker_done);
+                when start_bit_two =>
+                    state <= simple_state_transition(start_bit_two, start_end, recv_ticker_done);
+                when start_end =>
+                    state <= simple_state_transition(start_end, bit_start, recv_ticker_done);
+--bit_start, bit_read_one, bit_read_two, bit_end,
                 when bit_start =>
-                    ticks_passed := 0;
-                    if recv_ticker_done = '1' then
-                        state <= bit_read_one;
-                    else
-                        state <= bit_start;
-                    end if;
+                    state <= simple_state_transition(bit_start, bit_read_one, recv_ticker_done);
                 when bit_read_one =>
-                    if recv_ticker_done = '1' then
-                        state <= bit_read_two;
-                    else
-                        state <= bit_read_one;
-                    end if;
+                    state <= simple_state_transition(bit_read_one, bit_read_two, recv_ticker_done);
                 when bit_read_two =>
-                    if recv_ticker_done = '1' then
-                        state <= bit_end;
-                    else
-                        state <= bit_read_two;
-                    end if;
+                    state <= simple_state_transition(bit_read_two, bit_end, recv_ticker_done);
                 when bit_end =>
                     if recv_ticker_done = '1' then
                         bits_processed := bits_processed + 1;
@@ -130,11 +124,38 @@ begin
                     else
                         state <= bit_end;
                     end if;
+-- parity_start, parity_read_one, parity_read_two, parity_end,
+                when parity_start =>
+                    state <= simple_state_transition(parity_start, parity_read_one, recv_ticker_done);
+                when parity_read_one =>
+                    state <= simple_state_transition(parity_read_one, parity_read_two, recv_ticker_done);
+                when parity_read_two =>
+                    state <= simple_state_transition(parity_read_two, parity_end, recv_ticker_done);
+                when parity_end =>
+                    state <= simple_state_transition(parity_end, stop_start, recv_ticker_done);
+-- stop_start, stop_read_one, stop_read_two, stop_end);
+                when stop_start =>
+                    state <= simple_state_transition(stop_start, stop_bit_one, recv_ticker_done);
+                when stop_bit_one =>
+                    state <= simple_state_transition(stop_bit_one, stop_bit_two, recv_ticker_done);
+                when stop_bit_two =>
+                    if recv_ticker_done = '1' then
+                        stop_bits_processed := stop_bits_processed +1;
+                        if stop_bits_processed = stop_bits_in then
+                            state <= wait_start;
+                        else
+                            state <= stop_end;
+                        end if;
+                    else
+                        state <= stop_bit_two;
+                    end if;
+                when stop_end =>
+                    simple_state_transition(stop_end, stop_start, recv_ticker_done);
             end case;
         end if;
     end process;
 
-    process(state, uart_rx)
+    process(state)
     begin
     end process;
 end Behavioral;
