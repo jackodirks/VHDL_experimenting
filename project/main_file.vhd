996503e3cec5a995a6c6a82b5a4512e2b871fe21
Jacko Dirks <jacko.dirks@gmail.com>
2016-04-10 21:56:41 +0200

    Replaced the seperate uart components with the complete in main

--- project/main_file.vhd
+++ project/main_file.vhd
@@ -37,121 +37,91 @@ architecture Behavioral of main_file is
         );
     end component;
 
-    component uart_receiv is
+    component uart_main is
         generic (
-            baudrate                : Natural;
             clockspeed              : Natural;
-            parity_bit_in           : boolean;
-            parity_bit_in_type      : Natural range 0 to 3;
-            bit_count_in            : Natural range 5 to 9;
-            stop_bits_in            : Natural range 1 to 2
-        );
-        port (
-            rst                     : in    STD_LOGIC;
-            clk                     : in    STD_LOGIC;
-            uart_rx                 : in    STD_LOGIC;
-            received_data           : out   STD_LOGIC_VECTOR(8 DOWNTO 0);
-            data_ready              : out   STD_LOGIC;                    -- Signals that data has been received.
-            parity_error            : out   STD_LOGIC;                    -- Signals that the parity check has failed, is zero if there was none
-            data_error              : out   STD_LOGIC                     -- Signals that data receiving has encoutered errors
-        );
-    end component;
-
-    component uart_transmit is
-        generic (
             baudrate                : Natural;
-            clockspeed              : Natural;
             parity_bit_en           : boolean;
-            parity_bit_type         : Natural range 0 to 3;
-            bit_count               : Natural range 5 to 9;
-            stop_bits               : Natural range 1 to 2
+            parity_bit_type         : integer range 0 to 3;
+            bit_count               : integer range 5 to 9;
+            stop_bits_count         : integer range 1 to 2
         );
-        port (
-            rst                     : in    STD_LOGIC;
-            clk                     : in    STD_LOGIC;
-            uart_tx                 : out   STD_LOGIC;
-            data_in                 : in    STD_LOGIC_VECTOR(8 DOWNTO 0);
-            data_send_start         : in    STD_LOGIC;                    -- Signals that the data can now be send
-            ready                   : out   STD_LOGIC
+        Port (
+            rst                     : in STD_LOGIC;
+            clk                     : in STD_LOGIC;
+            uart_rx                 : in STD_LOGIC;
+            uart_tx                 : out STD_LOGIC;
+            send_start              : in STD_LOGIC;
+            data_in                 : in STD_LOGIC_VECTOR(8 DOWNTO 0);
+            data_out                : out STD_LOGIC_VECTOR(8 DOWNTO 0);
+            data_ready              : out STD_LOGIC;
+            data_error              : out STD_LOGIC;
+            parity_error            : out STD_LOGIC;
+            send_ready              : out STD_LOGIC
         );
-    end component;
 
-    component button_to_single_pulse is
-        generic (
-            debounce_ticks      : natural range 2 to natural'high
-        );
-        port (
-            clk                 : in STD_LOGIC;
-            rst                 : in STD_LOGIC;
-            pulse_in            : in STD_LOGIC;
-            pulse_out           : out STD_LOGIC
-        );
-    end component;
+        component button_to_single_pulse is
+            generic (
+                debounce_ticks      : natural range 2 to natural'high
+            );
+            port (
+                clk                 : in STD_LOGIC;
+                rst                 : in STD_LOGIC;
+                pulse_in            : in STD_LOGIC;
+                pulse_out           : out STD_LOGIC
+            );
+        end component;
 
-    component data_safe_8_bit is
-        port (
-            clk         : in STD_LOGIC;
-            rst         : in STD_LOGIC;
-            read        : in STD_LOGIC;
-            data_in     : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-            data_out    : out STD_LOGIC_VECTOR(7 DOWNTO 0)
-        );
-    end component;
+        component data_safe_8_bit is
+            port (
+                clk         : in STD_LOGIC;
+                rst         : in STD_LOGIC;
+                read        : in STD_LOGIC;
+                data_in     : in STD_LOGIC_VECTOR(7 DOWNTO 0);
+                data_out    : out STD_LOGIC_VECTOR(7 DOWNTO 0)
+            );
+        end component;
 
-    signal rst          : STD_LOGIC;
-    signal safe_data : STD_LOGIC_VECTOR(7 DOWNTO 0);
+        signal rst          : STD_LOGIC;
+        signal safe_data : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
-    signal uart_receive_done    : STD_LOGIC;
-    signal uart_received_data   : STD_LOGIC_VECTOR(8 DOWNTO 0);
-    signal uart_data_error      : STD_LOGIC;
-    signal uart_parity_error    : STD_LOGIC;
-    signal uart_rx              : STD_LOGIC;
-    signal uart_data_ready      : STD_LOGIC;
+        signal uart_receive_done    : STD_LOGIC;
+        signal uart_received_data   : STD_LOGIC_VECTOR(8 DOWNTO 0);
+        signal uart_data_error      : STD_LOGIC;
+        signal uart_parity_error    : STD_LOGIC;
+        signal uart_rx              : STD_LOGIC;
+        signal uart_data_ready      : STD_LOGIC;
 
-    signal uart_tx              : STD_LOGIC;
-    signal uart_send_data       : STD_LOGIC_VECTOR(8 DOWNTO 0) := (others => '0');
-    signal uart_start_send      : STD_LOGIC := '0';
-    signal uart_send_ready      : STD_LOGIC;
+        signal uart_tx              : STD_LOGIC;
+        signal uart_send_data       : STD_LOGIC_VECTOR(8 DOWNTO 0) := (others => '0');
+        signal uart_start_send      : STD_LOGIC := '0';
+        signal uart_send_ready      : STD_LOGIC;
 
-    signal debounce_pulse_in    : STD_LOGIC;
-    signal debounce_pulse_out   : STD_LOGIC;
+        signal debounce_pulse_in    : STD_LOGIC;
+        signal debounce_pulse_out   : STD_LOGIC;
 
 begin
-    uart_receiver : uart_receiv
+    uart : uart_main
     generic map (
-        baudrate => 115107,
-        clockspeed => 50000000,
-        parity_bit_in => true,
-        parity_bit_in_type => 1,
-        bit_count_in => 8,
-        stop_bits_in => 1
-    )
-    port map (
-        rst => rst,
-        clk => clk,
-        uart_rx => uart_rx,
-        received_data=> uart_received_data,
-        data_ready => uart_receive_done,
-        parity_error => uart_parity_error,
-        data_error => uart_data_error
-    );
-
-    uart_transmitter : uart_transmit
-    generic map (
-        baudrate => 115107,
-        clockspeed => 50000000,
-        parity_bit_en => true,
-        parity_bit_type => 1,
-        bit_count => 8,
-        stop_bits => 1
+        clockspeed          => 50000000,
+        baudrate            => 115107,
+        parity_bit_en       => true,
+        parity_bit_in_type  => 1,
+        bit_count           => 8,
+        stop_bits_count     => 1
     )
     port map (
         rst                 => rst,
         clk                 => clk,
+        uart_rx             => uart_rx,
         uart_tx             => uart_tx,
-        data_in             => uart_send_data,
-        data_send_start     => uart_start_send,
-        ready               => uart_send_ready
+        send_start          => uart_start_send,
+        data_in             => uart_received_data,
+        data_out            => uart_send_data,
+        data_ready          => uart_receive_done,
+        data_error          => uart_data_error,
+        parity_error        => uart_parity_error,
+        send_ready          => uart_send_ready
     );
 
     ss_driver : seven_segments_driver
