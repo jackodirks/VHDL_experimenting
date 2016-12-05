e3ab36110d863fadfc3856cd8876c1c6353d4b35
Jacko Dirks <jacko.dirks@gmail.com>
2016-12-05 10:25:32 +0100

    Removes non-needed g flags from make build

--- project/mk/flags.mk
+++ project/mk/flags.mk
@@ -1,6 +1,6 @@
 TIMESEED := $(shell date +%s)
 INCLUDEFLAGS = -i --work=$(WORKNAMEPREFIX) --std=$(STD) --workdir=$(WORKDIR)
-BUILDFLAGS = -m --work=$(WORKNAMEPREFIX) --std=$(STD) --workdir=$(WORKDIR) -gSEED=$(TIMESEED)
-RUNFLAGS = -r --work=$(WORKNAMEPREFIX) --std=$(STD) --workdir=$(WORKDIR) -gSEED=$(TIMESEED)
+BUILDFLAGS = -m --work=$(WORKNAMEPREFIX) --std=$(STD) --workdir=$(WORKDIR)
+RUNFLAGS = -r --work=$(WORKNAMEPREFIX) --std=$(STD) --workdir=$(WORKDIR)
 VIEWFLAGS = --stdout
 CLEANFLAGS = --remove --workdir=$(WORKDIR)
