96943fb1e41de31157e9860972e22f0186ea868e
Jacko Dirks <jacko.dirks@gmail.com>
2017-01-10 16:18:25 +0100

    Removes hstr function from ss tb, adds libref instead

--- project/sevenSegment/seven_segments_tb.vhd
+++ project/sevenSegment/seven_segments_tb.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.txt_util.all;
 
 entity seven_segments_tb is
     port (
@@ -9,45 +10,6 @@ entity seven_segments_tb is
 end seven_segments_tb;
 
 architecture Behavioral of seven_segments_tb is
-
-    function hstr(slv: std_logic_vector) return string is
-    variable hexlen: integer;
-    variable longslv : std_logic_vector(67 downto 0) := (others => '0');
-    variable hex : string(1 to 16);
-    variable fourbit : std_logic_vector(3 downto 0);
-    begin
-        hexlen := (slv'left+1)/4;
-        if (slv'left+1) mod 4 /= 0 then
-            hexlen := hexlen + 1;
-        end if;
-        longslv(slv'left downto 0) := slv;
-        for i in (hexlen -1) downto 0 loop
-            fourbit := longslv(((i*4)+3) downto (i*4));
-            case fourbit is
-                when "0000" => hex(hexlen -I) := '0';
-                when "0001" => hex(hexlen -I) := '1';
-                when "0010" => hex(hexlen -I) := '2';
-                when "0011" => hex(hexlen -I) := '3';
-                when "0100" => hex(hexlen -I) := '4';
-                when "0101" => hex(hexlen -I) := '5';
-                when "0110" => hex(hexlen -I) := '6';
-                when "0111" => hex(hexlen -I) := '7';
-                when "1000" => hex(hexlen -I) := '8';
-                when "1001" => hex(hexlen -I) := '9';
-                when "1010" => hex(hexlen -I) := 'A';
-                when "1011" => hex(hexlen -I) := 'B';
-                when "1100" => hex(hexlen -I) := 'C';
-                when "1101" => hex(hexlen -I) := 'D';
-                when "1110" => hex(hexlen -I) := 'E';
-                when "1111" => hex(hexlen -I) := 'F';
-                when "ZZZZ" => hex(hexlen -I) := 'z';
-                when "UUUU" => hex(hexlen -I) := 'u';
-                when "XXXX" => hex(hexlen -I) := 'x';
-                when others => hex(hexlen -I) := '?';
-            end case;
-        end loop;
-        return hex(1 to hexlen);
-    end hstr;
 
     procedure checkCorr(exp, got: STD_LOGIC_VECTOR) is
     begin
