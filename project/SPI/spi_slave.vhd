d2b686ffb62d0f08f89ac1076a4505bb2acd6625
Jacko Dirks <jacko.dirks@gmail.com>
2017-04-12 21:56:59 +0200

    Adds the first steps of the architecture of SPI slave

--- project/SPI/spi_slave.vhd
+++ project/SPI/spi_slave.vhd
@@ -19,11 +19,12 @@ entity spi_slave is
         sclk                    : in    STD_LOGIC;                          -- Serial clock
         mosi                    : in    STD_LOGIC;                          -- Master output slave input
         miso                    : out   STD_LOGIC;                          -- Master input slave output
+        ss                      : in    STD_LOGIC;                          -- Slave Select, if zero, this slave is selected.
         data_in                 : in    STD_LOGIC_VECTOR(127 DOWNTO 0);     -- Data to be transmitted
         data_out                : out   STD_LOGIC_VECTOR(127 DOWNTO 0);     -- Data that has been received
         block_size              : in    Natural range 1 to 32;              -- Data block size
         receive_block           : out   Natural range 0 to 127;             -- Last written block
-        transmit_block          : out   Natural range 0 to 127;             -- Block currently being read
+        transmit_block          : in    Natural range 0 to 127;             -- Block currently being read
         transmit_done           : out   STD_LOGIC;                          -- Signals that a full block was just transmitted
         receive_done            : out   STD_LOGIC                           -- Signals that data has been received.
     );
@@ -31,5 +32,35 @@ end uart_receiv;
 
 
 architecture Behavioral of spi_slave is
+    type state_type is (reset, wait_for_slave_select, wait_for_idle, data_get, data_set, block_done);
+
+    signal set_polarity         : STD_LOGIC := 0;
+    signal set_phase            : STD_LOGIC := 0;
+    signal set_block_size       : Natural range 1 to 32 := 32;
+
+    signal cur_read_block       : Natural range 0 to 127 := 0;
+    signal cur_write_block      : Natural range 0 to 127 := 0;
+
+    signal state                : state_type := reset;
+
+
 begin
+    -- State transition
+    process(clk, rst, mosi, sclk, ss)
+    begin
+        if rst = 1 then
+            state <= reset;
+        elsif rising_edge(clk) then
+            case state is
+                when reset =>
+                    state <= wait_for_slave_select;
+                when wait_for_slave_select =>
+                    if ss = 0 then
+                        state <= wait_for_idle;
+                    else
+                        state <= wait_for_slave_select;
+                    end if;
+            end case;
+        end if;
+    end process;
 end Behavioral;
