54a55ae0c973d65ea58979f62917f159a75077e3
Jacko Dirks <jacko.dirks@gmail.com>
2017-04-28 17:26:17 +0200

    Reworks main_tb for readability and test disabling

--- project/tb_main.vhd
+++ project/tb_main.vhd
@@ -48,52 +48,66 @@ architecture tb of tb_main is
 
     -- Constant declaration --
     constant clock_period                   : time := 20 ns;    -- Please make sure this number is divisible by 2.
-    constant test_count                     : natural := 5;
 
     -- Signal declaration --
     signal clk                              : STD_LOGIC := '0';
 
-    signal seven_segments_done              : boolean;
-    signal common_done                      : boolean;
-    signal uart_done                        : boolean;
+    signal seven_segments_done              : boolean := true;
+    signal seven_segments_success           : boolean := true;
 
-    signal seven_segments_success           : boolean;
-    signal common_success                   : boolean;
-    signal uart_success                     : boolean;
+    signal common_done                      : boolean := true;
+    signal common_success                   : boolean := true;
+
+    signal uart_success                     : boolean := true;
+    signal uart_done                        : boolean := true;
+
+
+    constant run_seven_segments_test        : boolean := false;
+    constant run_common_test                : boolean := false;
+    constant run_uart_test                  : boolean := false;
+    constant run_spi_test                   : boolean := true;
 
     signal randVal                          : natural := 0;
 begin
+    seven_segments_generate:
+    if run_seven_segments_test generate
+        seven_segments_test: seven_segments_tb
+        generic map (
+            clock_period => clock_period
+        )
+        port map (
+            clk => clk,
+            done => seven_segments_done,
+            success => seven_segments_success
+        );
+    end generate seven_segments_generate;
 
-    seven_segments_test: seven_segments_tb
-    generic map (
-        clock_period => clock_period
-    )
-    port map (
-        clk => clk,
-        done => seven_segments_done,
-        success => seven_segments_success
-    );
+    common_generate:
+    if run_common_test generate
+        common_test : common_tb
+        generic map (
+            clock_period => clock_period
+        )
+        port map (
+            clk => clk,
+            done => common_done,
+            success => common_success
+        );
+    end generate common_generate;
 
-    common_test : common_tb
-    generic map (
-        clock_period => clock_period
-    )
-    port map (
-        clk => clk,
-        done => common_done,
-        success => common_success
-    );
-
-    uart_test : uart_tb
-    generic map (
-        clock_period => clock_period,
-        randVal => randVal
-    )
-    port map (
-        clk => clk,
-        done => uart_done,
-        success => uart_success
-    );
+    uart_generate:
+    if run_uart_test generate
+        uart_test : uart_tb
+        generic map (
+            clock_period => clock_period,
+            randVal => randVal
+        )
+        port map (
+            clk => clk,
+            done => uart_done,
+            success => uart_success
+        );
+    end generate uart_generate;
 
     rand_gen : process
     begin
