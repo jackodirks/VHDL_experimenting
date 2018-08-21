1b3bb25c95c3043295264c3828498c33321ae1d4
Jacko Dirks <jacko.dirks@gmail.com>
2018-08-21 11:14:53 +0200

    Static SPI slave finished and tested for POL=PHA=0

--- project/SPI/spi_slave_static.vhd
+++ project/SPI/spi_slave_static.vhd
@@ -15,10 +15,9 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity spi_slave is
   generic (
-    FRAME_SIZE_BIT          : natural   := 16;
+    FRAME_SIZE_BIT_L2       : natural   := 4;
     POLARITY                : std_logic := '0';
-    PHASE                   : std_logic := '0';
-    AUTOCLEAR_FRAMES        : boolean   := true
+    PHASE                   : std_logic := '0'
   );
   port (
     rst                     : in  STD_LOGIC;
@@ -27,172 +26,69 @@ entity spi_slave is
     mosi                    : in  STD_LOGIC;                                    -- Master output slave input
     miso                    : out STD_LOGIC;                                    -- Master input slave output
     ss                      : in  STD_LOGIC;                                    -- Slave Select
-    trans_data              : in  std_logic_vector(FRAME_SIZE_BIT - 1 downto 0);
-    receiv_data             : out std_logic_vector(FRAME_SIZE_BIT - 1 downto 0);
-    done                    : out boolean;
-  );
+    trans_data              : in  std_logic_vector(2**FRAME_SIZE_BIT_L2 - 1 downto 0);
+    receiv_data             : out std_logic_vector(2**FRAME_SIZE_BIT_L2 - 1 downto 0);
+    done                    : out boolean
+);
 end spi_slave;
 
-
 architecture Behavioral of spi_slave is
-  type state_type is (reset, wait_for_slave_select, wait_for_idle, transaction, finished);
-  -- The state of the FSM
-  signal state                : state_type := reset;
-  signal
-
+  constant FRAME_SIZE_BIT     : natural := 2**FRAME_SIZE_BIT_L2;
+  signal transceiver_active   : boolean;
+  signal miso_int             : std_logic;
+  signal sclk_int             : std_logic;
+  signal cursor_ex            : unsigned(FRAME_SIZE_BIT_L2 - 1 downto 0);
 begin
-
-
-  -- The miso (slave output) controller, handles the MISO and data_in signals
-  miso_controller : process(clk, read_data_in, next_output, ss)
-      variable cursor         :   CURSOR_RANGE_SUBTYPE;
+  slave_selected : process(ss, miso_int, sclk)
   begin
-      if rising_edge(clk) then
-          if read_data_in then
-              data_in_buffer := data_in;
-              cursor := cur_block_size-1;
-          elsif next_output then
-              if cursor > 0 then
-                  cursor := cursor-1;
-              end if;
-          end if;
-      end if;
-      if ss = '1' then
-          miso <= 'Z';
-      else
-          miso <= data_in_buffer(cursor);
-      end if;
+    if ss = '1' then
+      miso <= 'Z';
+      sclk_int <= '0';
+    else
+      miso <= miso_int;
+      sclk_int <= sclk;
+    end if;
   end process;
 
-  -- State behaviour
-  state_behaviour: process(state)
+  finish_generator : process(clk, transceiver_active)
+    variable lastVal          : boolean := true;
   begin
-      case state is
-          when reset =>
-              lock_safe       <= false;
-              switch_buffer   <= false;
-              next_output     <= false;
-              next_input      <= false;
-              read_data_in    <= true;
-          when wait_for_slave_select|wait_for_idle =>
-              lock_safe       <= true;
-              switch_buffer   <= false;
-              next_output     <= false;
-              next_input      <= false;
-              read_data_in    <= true;
-          when data_get_wait|data_set_wait =>
-              lock_safe       <= true;
-              switch_buffer   <= false;
-              next_output     <= false;
-              next_input      <= false;
-              read_data_in    <= false;
-          when data_get =>
-              lock_safe       <= true;
-              switch_buffer   <= false;
-              next_output     <= false;
-              next_input      <= true;
-              read_data_in    <= false;
-          when data_set =>
-              lock_safe       <= true;
-              switch_buffer   <= false;
-              next_output     <= true;
-              next_input      <= false;
-              read_data_in    <= false;
-          when block_finished =>
-              lock_safe       <= true;
-              switch_buffer   <= true;
-              next_output     <= false;
-              next_input      <= false;
-              read_data_in    <= true;
-      end case;
+    if rising_edge(clk) then
+      lastVal := not transceiver_active;
+    end if;
+    done <= not (transceiver_active or lastVal);
   end process;
 
-  -- State transition
-  state_transition: process(clk, rst, mosi, sclk, ss)
-      variable prev_sclk  : STD_LOGIC;
-      variable cur_sclk   : STD_LOGIC;
-      variable cur_bit    : BIT_RANGE_SUBTYPE;
+  transceiver : process(clk, trans_data)
+    variable cursor           : unsigned(FRAME_SIZE_BIT_L2 - 1 downto 0) := (others => '0');
+    variable data_out         : std_logic_vector(receiv_data'RANGE) := (others => '0');
+    variable last_sclk        : std_logic := '0';
   begin
+    if rising_edge(clk) then
       if rst = '1' then
-          state <= reset;
-          cur_bit := 0;
-      elsif rising_edge(clk) then
-          prev_sclk := cur_sclk;
-          cur_sclk := sclk_debounced;
-          case state is
-              when reset|wait_for_slave_select =>
-                  if ss = '1' then
-                      state <= wait_for_slave_select;
-                  else
-                      state <= wait_for_idle;
-                  end if;
-              when wait_for_idle =>
-                  -- possible situations:
-                  -- Polarity = 0, sclk = 0, phase = 0: go to data_get
-                  -- Polarity = 0, sclk = 0, phase = 1: go to data_set
-                  -- Polarity = 1, sclk = 1, phase = 0: go to data_set
-                  -- Polarity = 1, sclk = 1, phase = 1: go to data_get
-                  -- Polarity != sclk: stay in wait_for_idle
-                  if cur_polarity /= cur_sclk then
-                      state <= wait_for_idle;
-                  elsif cur_polarity = '0' then
-                      if cur_phase = '0' then
-                          state <= data_get_wait;
-                      else
-                          state <= data_set_wait;
-                      end if;
-                  else
-                      if cur_phase = '1' then
-                          state <= data_get_wait;
-                      else
-                          state <= data_set_wait;
-                      end if;
-                  end if;
-              when data_get_wait =>
-                  -- Wait for the next edge
-                  if prev_sclk /= cur_sclk then
-                      state <= data_get;
-                  else
-                      state <= data_get_wait;
-                  end if;
-              when data_get =>
-                  if cur_phase /= cur_polarity then
-                      if cur_bit+1 = cur_block_size then
-                          state <= block_finished;
-                      else
-                          state <= data_set_wait;
-                          cur_bit := cur_bit + 1;
-                      end if;
-                  else
-                      state <= data_set_wait;
-                  end if;
-              when data_set_wait =>
-                  if prev_sclk /= cur_sclk then
-                      state <= data_set;
-                  else
-                      state <= data_set_wait;
-                  end if;
-              when data_set =>
-                  if cur_phase = cur_polarity then
-                      if cur_bit+1 >= cur_block_size then
-                          state <= block_finished;
-                      else
-                          cur_bit := cur_bit + 1;
-                          state <= data_get_wait;
-                      end if;
-                  else
-                      state <= data_get_wait;
-                  end if;
-              when block_finished =>
-                  cur_bit := 0;
-                  if ss = '1' then
-                      state <= wait_for_slave_select;
-                  elsif cur_phase = cur_polarity then
-                      state <= data_get_wait;
-                  else
-                      state <= data_set_wait;
-                  end if;
-          end case;
+        if POLARITY = PHASE then
+          cursor := (others => '1');
+        else
+          cursor := (others => '0');
+        end if;
+        last_sclk := sclk;
+      elsif sclk_int /= last_sclk then
+        last_sclk := sclk_int;
+        if (PHASE = '0' and sclk_int = '0') or (PHASE = '1' and sclk_int = '1') then
+          cursor := cursor - 1;
+        end if;
+        if (PHASE = '0' and sclk_int = '1') or (PHASE = '1' and sclk_int = '0') then
+          data_out(to_integer(cursor)) := mosi;
+        end if;
       end if;
+    end if;
+    if POLARITY = PHASE then
+      transceiver_active <= not(cursor = 2**FRAME_SIZE_BIT_L2 - 1);
+    else
+      transceiver_active <= not(cursor = 0);
+    end if;
+    receiv_data <= data_out;
+    miso_int <= trans_data(to_integer(cursor));
+    cursor_ex <= cursor;
   end process;
 end Behavioral;
--- project/SPI/spi_tb.vhd
+++ project/SPI/spi_tb.vhd
@@ -18,92 +18,95 @@ end spi_tb;
 
 architecture Behavioral of spi_tb is
 
-    constant clk_freq                       : natural := (1000 ms / clock_period);
-    constant sclk_period                    : time := clock_period * 8;        -- Run eight times slower then the system clock
-    constant half_sclk_period               : time := sclk_period / 2;          -- Convinience thing
+    constant sclk_period      : time := clock_period * 2;        -- Run eight times slower than the system clock
+    constant FRAME_SIZE_L2    : natural := 1;
+    constant FRAME_SIZE       : natural := 2**FRAME_SIZE_L2;
 
     -- inout signals slave 1
-    signal slave_1_rst          : STD_LOGIC;
-    signal slave_1_polarity     : STD_LOGIC;
-    signal slave_1_phase        : STD_LOGIC;
-    signal slave_1_sclk         : STD_LOGIC;
-    signal slave_1_mosi         : STD_LOGIC;
-    signal slave_1_miso         : STD_LOGIC;
-    signal slave_1_ss           : STD_LOGIC;
-    signal slave_1_data_in      : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    signal slave_1_data_out     : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    signal slave_1_block_size   : natural range 1 to 32;
-    signal slave_1_block_done   : boolean;
+    signal slave_1_rst        : STD_LOGIC;
+    signal slave_1_sclk       : STD_LOGIC;
+    signal slave_1_mosi       : STD_LOGIC;
+    signal slave_1_miso       : STD_LOGIC;
+    signal slave_1_ss         : STD_LOGIC;
+    signal slave_1_data_in    : STD_LOGIC_VECTOR(FRAME_SIZE - 1 downto 0);
+    signal slave_1_data_out   : STD_LOGIC_VECTOR(FRAME_SIZE - 1 downto 0);
+    signal slave_1_done       : boolean;
     -- meta signals slave 1
-    signal spi_slave_1_suc      : boolean := true;
-    signal spi_slave_1_done     : boolean := false;
+    signal spi_slave_1_done   : boolean;
+    signal spi_slave_1_suc   : boolean := true;
 
 begin
 
     success <= spi_slave_1_suc;
     done <= spi_slave_1_done;
 
+    -- Geberics are kept default
+    -- Frame size 16 bit
+    -- SPO = SPH = 0
     spi_slave_1 : entity work.spi_slave
     generic map (
-        debounce_ticks => 0
+      FRAME_SIZE_BIT_L2 => FRAME_SIZE_L2
     )
     port map (
         rst => slave_1_rst,
         clk => clk,
-        polarity => slave_1_polarity,
-        phase => slave_1_phase,
         sclk => slave_1_sclk,
         mosi => slave_1_mosi,
         miso => slave_1_miso,
         ss => slave_1_ss,
-        data_in => slave_1_data_in,
-        data_out => slave_1_data_out,
-        block_size => slave_1_block_size,
-        block_done => slave_1_block_done
+        trans_data => slave_1_data_in,
+        receiv_data => slave_1_data_out,
+        done => slave_1_done
     );
 
     slave_1_test : process
-        variable cur_data_out : STD_LOGIC_VECTOR(4 DOWNTO 0);
+        variable cur_data_out : STD_LOGIC_VECTOR(FRAME_SIZE - 1 downto 0);
     begin
         slave_1_rst <= '1';
         slave_1_ss <= '0';
-        slave_1_polarity <= '0';
-        slave_1_phase <= '0';
-        slave_1_block_size <= 5;
-        slave_1_sclk <= '1';
+        slave_1_sclk <= '0';
         slave_1_data_in <= (others => '0');
         slave_1_mosi <= '0';
+        wait for clock_period/2;
         -- Easy opener, run the values 0-15 trough both in- and output and see if it works as expected. 0 is selected already
         -- Wait for 2 cycles
-        for D in 0 to 1 loop
-            wait until rising_edge(clk);
-        end loop;
+        wait for 2*clock_period;
         -- Drop the reset
         slave_1_rst <= '0';
         -- Wait for 2 cycles
-        for D in 0 to 1 loop
-            wait until rising_edge(clk);
-        end loop;
-        wait for half_sclk_period;
-        slave_1_sclk <= not slave_1_sclk;
-        for D in 0 to 31 loop
+        wait for 2*clock_period;
+        wait for sclk_period/2;
+        for D in 0 to 3 loop
             cur_data_out := STD_LOGIC_VECTOR(to_unsigned(D, cur_data_out'length));
             -- The system should be waiting for the first sclk, after which it tries to read data
-            slave_1_data_in(4 DOWNTO 0) <= cur_data_out;
-            for B in 4 downto 0 loop
-                -- Set/write
-                slave_1_mosi <= cur_data_out(B);
-                wait for half_sclk_period;
-                slave_1_sclk <= not slave_1_sclk;
+            slave_1_data_in <= cur_data_out;
+            -- Pre set
+            slave_1_mosi <= cur_data_out(FRAME_SIZE - 1);
+            for B in FRAME_SIZE - 2 downto 0 loop
                 -- Get/read
-                if slave_1_miso /= cur_data_out(B) then
-                   spi_slave_1_suc <= false;
-                   report "slave 1 read error: expected " & STD_LOGIC'image(cur_data_out(B)) & " but got: " & std_logic'image(slave_1_miso) & " B = " & natural'image(B) & " D = " & natural'image(D) severity error;
-               end if;
-                wait for half_sclk_period;
+                wait for sclk_period/2;
                 slave_1_sclk <= not slave_1_sclk;
+                assert(slave_1_miso = cur_data_out(B + 1));
+                -- Set/write
+                wait for sclk_period/2;
+                slave_1_sclk <= not slave_1_sclk;
+                slave_1_mosi <= cur_data_out(B);
             end loop;
+            -- Get/read
+            wait for sclk_period/2;
+            slave_1_sclk <= not slave_1_sclk;
+            assert(slave_1_miso = cur_data_out(0));
+            -- Finishing edge
+            wait for sclk_period/2;
+            slave_1_sclk <= not slave_1_sclk;
+            slave_1_mosi <= '0';
+            wait for (1.5)*clock_period;
+            assert slave_1_done = true;
+            assert(slave_1_data_out = std_logic_vector(to_unsigned(D, slave_1_data_out'LENGTH)));
+            wait for sclk_period - (1.5)*clock_period;
+            wait for sclk_period;
         end loop;
+        slave_1_sclk <= '0';
         spi_slave_1_done <= true;
         wait;
     end process;
