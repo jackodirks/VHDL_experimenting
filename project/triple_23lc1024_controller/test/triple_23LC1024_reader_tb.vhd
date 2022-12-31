e3179e65fefb482bf952e38dd329ef95a5c5d2f6
Jacko Dirks <jacko.dirks@gmail.com>
2022-12-31 15:32:57 +0100

    triple_23lc1024_reader: Handle faults

--- project/triple_23lc1024_controller/test/triple_23LC1024_reader_tb.vhd
+++ project/triple_23lc1024_controller/test/triple_23LC1024_reader_tb.vhd
@@ -34,10 +34,11 @@ architecture tb of triple_23LC1024_reader_tb is
     signal active : boolean;
     signal ready :  std_logic := '0';
     signal valid : std_logic;
-    signal fault : boolean := false;
     signal address : bus_address_type := (others => '0');
     signal read_data : bus_data_type := (others => '0');
     signal burst : std_logic := '0';
+    signal fault : std_logic;
+    signal faultData : std_logic_vector(bus_fault_type'range);
 
     signal cs_set : std_logic;
     signal cs_state : std_logic;
@@ -64,7 +65,7 @@ begin
                 burst <= '0';
                 wait until rising_edge(clk) and valid = '1';
                 ready <= '0';
-                address <= (others => 'Z');
+                address <= (others => 'X');
                 check_equal(read_data, std_logic_vector(to_unsigned(255, bus_data_type'length)));
                 check(active);
                 wait until rising_edge(clk);
@@ -80,7 +81,7 @@ begin
                 burst <= '0';
                 wait until rising_edge(clk) and valid = '1';
                 ready <= '0';
-                address <= (others => 'Z');
+                address <= (others => 'X');
                 check_equal(read_data, exp_data);
                 check(active);
                 wait until rising_edge(clk);
@@ -101,7 +102,7 @@ begin
                 wait until rising_edge(clk) and valid = '1';
                 check_equal(read_data, std_logic_vector(to_unsigned(14, bus_data_type'length)));
                 ready <= '0';
-                address <= (others => 'Z');
+                address <= (others => 'X');
                 check(active);
                 wait until not active;
             elsif run("Burst read from address zero which contains 254 and address 4 which contains 14 results in 254, 14") then
@@ -120,7 +121,7 @@ begin
                 wait until rising_edge(clk) and valid = '1';
                 check_equal(read_data, std_logic_vector(to_unsigned(14, bus_data_type'length)));
                 ready <= '0';
-                address <= (others => 'Z');
+                address <= (others => 'X');
                 check(active);
                 wait until not active;
             elsif run("Paused burst read from address zero which contains 254 and address 4 which contains 14 results in 254, 14") then
@@ -136,25 +137,66 @@ begin
                 address <= std_logic_vector(to_unsigned(4, address'length));
                 burst <= '0';
                 ready <= '0';
-                wait until rising_edge(clk) and valid = '1';
-                wait for 20 * clk_period;
+                wait for 2 us;
+                wait until falling_edge(clk);
                 ready <= '1';
+                wait until rising_edge(clk) and valid = '1';
                 check_equal(read_data, std_logic_vector(to_unsigned(14, bus_data_type'length)));
-                wait until rising_edge(clk) and valid = '0';
                 ready <= '0';
-                address <= (others => 'Z');
+                address <= (others => 'X');
                 check(active);
                 wait until not active;
-            elsif run("Faults should end burst") then
+            elsif run("Unaligned address error is detected") then
+                address <= std_logic_vector(to_unsigned(1, address'length));
+                rst <= '0';
+                ready <= '1';
+                wait until rising_edge(clk) and fault = '1';
+                check_equal(faultData, bus_fault_unaligned_access);
+            elsif run("Illegal burst error is detected") then
+                address <= (others => '0');
+                address(16 downto 0) <= std_logic_vector(to_unsigned(16#1fffc#, 17));
+                burst <= '1';
+                ready <= '1';
+                rst <= '0';
+                wait until rising_edge(clk) and fault = '1';
+                check_equal(faultData, bus_fault_illegal_address_for_burst);
+            elsif run("Reader moves on after error") then
                 set_all_mode(SeqMode, SqiMode, actor, net);
+                write_bus_word(net, actor, std_logic_vector(to_unsigned(0, 17)), std_logic_vector(to_unsigned(254, bus_data_type'length)));
+                write_bus_word(net, actor, std_logic_vector(to_unsigned(4, 17)), std_logic_vector(to_unsigned(14, bus_data_type'length)));
+                address <= std_logic_vector(to_unsigned(1, address'length));
+                rst <= '0';
+                ready <= '1';
+                wait until rising_edge(clk) and fault = '1';
+                address <= std_logic_vector(to_unsigned(4, address'length));
+                wait until rising_edge(clk) and valid = '1';
+                check_equal(read_data, std_logic_vector(to_unsigned(14, bus_data_type'length)));
+            elsif run("Errors end burst") then
+                set_all_mode(SeqMode, SqiMode, actor, net);
+                write_bus_word(net, actor, std_logic_vector(to_unsigned(0, 17)), std_logic_vector(to_unsigned(254, bus_data_type'length)));
+                write_bus_word(net, actor, std_logic_vector(to_unsigned(4, 17)), std_logic_vector(to_unsigned(14, bus_data_type'length)));
                 address <= std_logic_vector(to_unsigned(0, address'length));
                 rst <= '0';
                 ready <= '1';
                 burst <= '1';
-                fault <= false;
                 wait until rising_edge(clk) and valid = '1';
+                address <= std_logic_vector(to_unsigned(1, address'length));
+                wait until rising_edge(clk) and fault = '1';
+                check_equal(faultData, bus_fault_unaligned_access);
+                wait until rising_edge(cs_n(0));
+            elsif run("Active remains asserted until CS = 1") then
+                set_all_mode(SeqMode, SqiMode, actor, net);
+                address <= std_logic_vector(to_unsigned(0, address'length));
+                rst <= '0';
+                ready <= '1';
+                wait until rising_edge(clk) and valid = '1';
+                wait until rising_edge(clk);
+                check_equal(active, true);
                 ready <= '0';
-                fault <= true;
+                wait until not active or rising_edge(cs_n(0));
+                if cs_n(0) = '0' then
+                    check_equal(active, true);
+                end if;
                 wait until not active;
             end if;
         end loop;
@@ -206,6 +248,7 @@ begin
         fault => fault,
         address => address,
         read_data => read_data,
-        burst => burst
+        burst => burst,
+        faultData => faultData
     );
 end tb;
--- project/triple_23lc1024_controller/triple_23lc1024_reader.vhd
+++ project/triple_23lc1024_controller/triple_23lc1024_reader.vhd
@@ -23,11 +23,12 @@ entity triple_23lc1024_reader is
         ready : in std_logic;
         valid : out std_logic;
         active : out boolean;
-        fault : in boolean;
+        fault : out std_logic;
 
         address : in bus_address_type;
         read_data : out bus_data_type;
-        burst : in std_logic
+        burst : in std_logic;
+        faultData : out bus_fault_type
     );
 end triple_23lc1024_reader;
 
@@ -36,11 +37,28 @@ architecture behavioral of triple_23lc1024_reader is
 
     signal half_period_timer_rst : std_logic := '1';
     signal half_period_timer_done : std_logic;
+
+    procedure detect_fault (
+        variable fault_internal : out std_logic;
+        signal faultData_internal : out std_logic_vector(faultData'range))
+    is
+        constant expBusAlignment : std_logic_vector(bus_bytes_per_word_log2b - 1 downto 0) := (others => '0');
+    begin
+        fault_internal := '0';
+        if address(expBusAlignment'range) /= expBusAlignment then
+            fault_internal := '1';
+            faultData_internal <= bus_fault_unaligned_access;
+        elsif burst = '1' and not is_address_legal_for_burst(address) then
+            fault_internal := '1';
+            faultData_internal <= bus_fault_illegal_address_for_burst;
+        end if;
+    end detect_fault;
 begin
 
     process(clk)
         variable count : natural := 0;
         variable cs_set_internal : std_logic := '1';
+        variable fault_internal : std_logic := '0';
         variable read_data_internal : bus_data_type := (others => '0');
         variable transmitCommandAndAddress : std_logic_vector(31 downto 0) := (others => '0');
         constant max_count : natural := 20 + 2**(bus_data_width_log2b - 2) * 2;
@@ -66,56 +84,59 @@ begin
                     spi_clk <= '1';
                 end if;
 
+                if cs_set_internal = '0' then
+                    active <= true;
+                else
+                    active <= false;
+                end if;
+
                 if count = 0 then
                     transaction_complete := false;
                     half_period_timer_rst <= '1';
                     spi_sio <= (others => 'Z');
                     valid <= '0';
-                    if cs_set_internal = '1' then
-                        active <= false;
-                    else
-                        active <= true;
-                    end if;
-                    if cs_set_internal = '1' and ready = '1' then
-                        cs_set_internal := '0';
+                    if fault_internal = '1' then
+                        fault_internal := '0';
+                    elsif cs_set_internal = '1' and ready = '1' then
                         transmitCommandAndAddress := instructionRead & "0000000" & address(16 downto 0);
-                        active <= true;
-                    end if;
-                    if cs_set_internal = '0' and cs_state = '0' then
+                        detect_fault(fault_internal => fault_internal, faultData_internal => faultData);
+                        if fault_internal = '0' then
+                            cs_set_internal := '0';
+                        end if;
+                    elsif cs_set_internal = '0' and cs_state = '0' then
                         spi_sio <= transmitCommandAndAddress(transmitCommandAndAddress'high downto transmitCommandAndAddress'high - 3);
                         half_period_timer_rst <= '0';
-                        active <= true;
                     end if;
                 end if;
 
-                if count >= 1 and count <= 15 then
+                if count > 0 then
                     half_period_timer_rst <= '0';
                     cs_set_internal := '0';
-                    active <= true;
                     valid <= '0';
+                    fault_internal := '0';
+                end if;
+
+                if count >= 1 and count <= 15 then
                     if count mod 2 = 0 and half_period_timer_done = '1' then
                         transmitCommandAndAddress := std_logic_vector(shift_left(unsigned(transmitCommandAndAddress), 4));
                     end if;
                     spi_sio <= transmitCommandAndAddress(transmitCommandAndAddress'high downto transmitCommandAndAddress'high - 3);
                 end if;
 
-                -- now we get a dummy byte
-                if count >= 16 and count <= 20 then
-                    transaction_complete := false;
+                if count > 15 then
                     half_period_timer_rst <= '0';
                     cs_set_internal := '0';
-                    active <= true;
                     valid <= '0';
+                    fault_internal := '0';
                     spi_sio <= (others => 'Z');
+                end if;
+                -- now we get a dummy byte. Also the point where we continue from when doing a burst.
+                if count >= 16 and count <= 20 then
+                    transaction_complete := false;
                 end if;
 
                 -- Now the incoming data, which we read on the rising edge (= when count is uneven)
                 if count > 20 and count < max_count - 1 then
-                    half_period_timer_rst <= '0';
-                    cs_set_internal := '0';
-                    active <= true;
-                    valid <= '0';
-                    spi_sio <= (others => 'Z');
                     if count mod 2 = 1 and half_period_timer_done = '1' then
                         read_data_internal(read_data_internal'high downto read_data_internal'high - 3) := spi_sio;
                     end if;
@@ -125,22 +146,15 @@ begin
                 end if;
 
                 if count = max_count - 1 then
-                    half_period_timer_rst <= '0';
-                    cs_set_internal := '0';
-                    active <= true;
-                    half_period_timer_rst <= '0';
-                    valid <= '1';
-                    if transaction_complete then
-                        valid <= '0';
-                    end if;
-                    spi_sio <= (others => 'Z');
                     if half_period_timer_done = '1' then
                         read_data_internal(read_data_internal'high downto read_data_internal'high - 3) := spi_sio;
                     end if;
-                    if not transaction_complete and not fault then
+                    if not transaction_complete then
                         if ready = '1' then
+                            valid <= '1';
                             transaction_complete := true;
-                            if burst = '1' then
+                            detect_fault(fault_internal => fault_internal, faultData_internal => faultData);
+                            if fault_internal = '0' and burst = '1' then
                                 count := 19;
                             end if;
                         else
@@ -152,18 +166,17 @@ begin
                 if count = max_count then
                     half_period_timer_rst <= '1';
                     cs_set_internal := '1';
-                    spi_sio <= (others => 'Z');
-                    valid <= '0';
                     transaction_complete := false;
+                    active <= true;
                     if cs_state = '1' then
                         count := 0;
-                        active <= false;
                     end if;
                 end if;
             end if;
         end if;
         cs_set <= cs_set_internal;
         read_data <= reorder_nibbles(read_data_internal);
+        fault <= fault_internal;
     end process;
 
     half_period_timer : entity work.simple_multishot_timer
