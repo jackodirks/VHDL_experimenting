b0858aea9ef8031ee510ef8aa29e9bc18edb107e
Jacko Dirks <jacko.dirks@gmail.com>
2022-12-31 16:20:10 +0100

    Introduce triple_23lc1024_cs_control

---
+++ project/triple_23lc1024_controller/test/triple_23LC1024_cs_control_tb.vhd
@@ -0,0 +1,81 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+library tb;
+library src;
+
+entity triple_23LC1024_cs_control_tb is
+    generic (
+        runner_cfg : string);
+end entity;
+
+architecture tb of triple_23LC1024_cs_control_tb is
+    constant clk_period : time := 20 ns;
+    constant spi_cs_setup_ticks : natural := 2;
+    constant spi_cs_hold_ticks : natural := 3;
+
+    signal clk : std_logic := '0';
+    signal cs_set : std_logic := '1';
+    signal cs_state : std_logic;
+    signal cs_n_requested : std_logic_vector(1 downto 0) := (others => '0');
+    signal spi_cs_n : std_logic_vector(2 downto 0);
+begin
+    clk <= not clk after (clk_period/2);
+
+    process
+        variable exp_cs_n : std_logic_vector(spi_cs_n'range) := (others => '1');
+    begin
+        test_runner_setup(runner, runner_cfg);
+        while test_suite loop
+            if run("Default values are sane") then
+                check_equal(cs_state, '1');
+                exp_cs_n := (others => '1');
+                check_equal(spi_cs_n, exp_cs_n);
+            elsif run("Input 00 gives output 110") then
+                cs_n_requested <= (others => '0');
+                cs_set <= '0';
+                wait until rising_edge(clk) and cs_state = '0';
+                exp_cs_n := "110";
+                check_equal(spi_cs_n, exp_cs_n);
+            elsif run("Device cycles back to 111") then
+                cs_n_requested <= "01";
+                cs_set <= '0';
+                wait until rising_edge(clk) and cs_state = '0';
+                exp_cs_n := "101";
+                check_equal(spi_cs_n, exp_cs_n);
+                cs_set <= '1';
+                exp_cs_n := "111";
+                wait until rising_edge(clk) and cs_state = '1';
+                check_equal(spi_cs_n, exp_cs_n);
+            end if;
+        end loop;
+        wait for 2*clk_period;
+        test_runner_cleanup(runner);
+        wait;
+    end process;
+
+    test_runner_watchdog(runner,  100 us);
+
+    mem_pcb : entity tb.triple_M23LC1024
+    port map (
+        cs_n => spi_cs_n,
+        sck => 'Z'
+    );
+
+    cs_control : entity src.triple_23lc1024_cs_control
+    generic map (
+        spi_cs_setup_ticks => spi_cs_setup_ticks,
+        spi_cs_hold_ticks => spi_cs_hold_ticks
+    ) port map (
+        clk => clk,
+        cs_set => cs_set,
+        cs_state => cs_state,
+        cs_n_requested => cs_n_requested,
+        spi_cs_n => spi_cs_n
+    );
+    
+end architecture;
---
+++ project/triple_23lc1024_controller/triple_23lc1024_cs_control.vhd
@@ -0,0 +1,86 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity triple_23lc1024_cs_control is
+    generic (
+        spi_cs_setup_ticks : natural;
+        spi_cs_hold_ticks : natural
+    );
+    port (
+        clk : in std_logic;
+
+        cs_set : in std_logic;
+        cs_state : out std_logic;
+
+        cs_n_requested : in std_logic_vector(1 downto 0);
+        spi_cs_n : out std_logic_vector(2 downto 0)
+    );
+end triple_23lc1024_cs_control;
+
+architecture behavioral of triple_23lc1024_cs_control is
+
+    signal setup_timer_rst : std_logic := '1';
+    signal setup_timer_done : std_logic;
+
+    signal hold_timer_rst : std_logic := '1';
+    signal hold_timer_done : std_logic;
+begin
+
+    process(clk)
+        variable cs_state_internal : std_logic := '1';
+        variable spi_cs_n_internal : std_logic_vector(spi_cs_n'range) := (others => '1');
+        variable spi_cs_n_decoded : std_logic_vector(spi_cs_n'range) := (others => '1');
+    begin
+        if rising_edge(clk) then
+            case cs_n_requested is
+                when "00" =>
+                    spi_cs_n_decoded := "110";
+                when "01" =>
+                    spi_cs_n_decoded := "101";
+                when "10" =>
+                    spi_cs_n_decoded := "011";
+                when others =>
+                    spi_cs_n_decoded := "111";
+            end case;
+
+            setup_timer_rst <= '1';
+            hold_timer_rst <= '1';
+            if cs_set = '0' and cs_state_internal = '1' then
+                spi_cs_n_internal := spi_cs_n_decoded;
+                setup_timer_rst <= '0';
+                if setup_timer_done = '1' then
+                    cs_state_internal := '0';
+                end if;
+            elsif cs_set = '1' and cs_state_internal = '0' then
+                hold_timer_rst <= '0';
+                if hold_timer_done = '1' then
+                    cs_state_internal := '1';
+                    spi_cs_n_internal := (others => '1');
+                end if;
+            end if;
+        end if;
+        cs_state <= cs_state_internal;
+        spi_cs_n <= spi_cs_n_internal;
+    end process;
+
+    setup_timer : entity work.simple_multishot_timer
+    generic map (
+        match_val => spi_cs_setup_ticks
+    )
+    port map (
+        clk => clk,
+        rst => setup_timer_rst,
+        done => setup_timer_done
+    );
+
+    hold_timer : entity work.simple_multishot_timer
+    generic map (
+        match_val => spi_cs_hold_ticks
+    )
+    port map (
+        clk => clk,
+        rst => hold_timer_rst,
+        done => hold_timer_done
+    );
+end behavioral;
