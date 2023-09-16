f3e5d5b6a9f649372731fffbfd3a2c00af5c554d
Jacko Dirks <jacko.dirks@gmail.com>
2023-09-16 11:24:52 +0200

    Remove unused VHDL and depp slave/master

--- project/run.py
+++ project/run.py
@@ -18,9 +18,6 @@ src_library.add_source_files(SRC_PATH / "main_file.vhd")
 src_library.add_source_files(SRC_PATH / "bus" / "*.vhd")
 tb_library.add_source_files(SRC_PATH / "bus" / "test" / "*.vhd")
 
-src_library.add_source_files(SRC_PATH / "deppSlave" / "*.vhd")
-tb_library.add_source_files(SRC_PATH / "deppSlave" / "test" / "*.vhd")
-
 src_library.add_source_files(SRC_PATH / "sevenSegment" / "*.vhd")
 tb_library.add_source_files(SRC_PATH / "sevenSegment" / "test" / "*.vhd")
 
