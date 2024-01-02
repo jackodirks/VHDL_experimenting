2e29cd5c6d31b33297cbac39a2b7400b6b14716c
Jacko Dirks <jacko.dirks@gmail.com>
2024-01-02 12:45:53 +0100

    uart_bus_master: Refactor

--- project/uart_bus_master/uart_bus_master.vhd
+++ project/uart_bus_master/uart_bus_master.vhd
@@ -34,24 +34,6 @@ architecture behaviourial of uart_bus_master is
     signal rx_byte : std_logic_vector(7 downto 0);
     signal rx_data_ready : boolean;
 
-    pure function translateCommand(byte : std_logic_vector(7 downto 0))
-                                   return command_type is
-        variable ret_val : command_type := no_command;
-    begin
-        case byte is
-            when uart_bus_master_pkg.COMMAND_READ_WORD =>
-                ret_val := command_read_word;
-            when uart_bus_master_pkg.COMMAND_WRITE_WORD =>
-                ret_val := command_write_word;
-            when uart_bus_master_pkg.COMMAND_WRITE_WORD_SEQUENCE =>
-                ret_val := command_write_word_sequence;
-            when others =>
-                ret_val := no_command;
-        end case;
-        return ret_val;
-    end function;
-
-
     -- tx_queue_signals
     signal tx_queue_data_in : std_logic_vector(7 downto 0) := (others => '0');
     signal tx_queue_push_data : boolean := false;
@@ -79,6 +61,112 @@ architecture behaviourial of uart_bus_master is
     signal sequence_size_buf : natural range 0 to 255;
     signal state_buf : state_type;
 
+    pure function translateCommand(byte : std_logic_vector(7 downto 0))
+                                   return command_type is
+        variable ret_val : command_type := no_command;
+    begin
+        case byte is
+            when uart_bus_master_pkg.COMMAND_READ_WORD =>
+                ret_val := command_read_word;
+            when uart_bus_master_pkg.COMMAND_WRITE_WORD =>
+                ret_val := command_write_word;
+            when uart_bus_master_pkg.COMMAND_WRITE_WORD_SEQUENCE =>
+                ret_val := command_write_word_sequence;
+            when others =>
+                ret_val := no_command;
+        end case;
+        return ret_val;
+    end function;
+
+    procedure rx_queue_to_word(
+        variable queue_wait_cycle : inout boolean;
+        variable word_index_counter : inout natural range 0 to 3;
+        signal word : out std_logic_vector(31 downto 0);
+        signal byte : in std_logic_vector(7 downto 0);
+        signal queue_empty : in boolean;
+        signal queue_pop : out boolean;
+        variable word_complete : out boolean) is
+    begin
+        word_complete := false;
+        if queue_wait_cycle then
+            queue_wait_cycle := false;
+        elsif not queue_empty then
+            word(word_index_counter*8 + 7 downto word_index_counter*8) <= byte;
+            queue_pop <= true;
+            queue_wait_cycle := true;
+            if word_index_counter = 3 then
+                word_complete := true;
+                word_index_counter := 0;
+            else
+                word_index_counter := word_index_counter + 1;
+                word_complete := false;
+            end if;
+        end if;
+    end procedure;
+
+    procedure rx_queue_to_byte(
+        variable queue_wait_cycle : inout boolean;
+        variable byte_out : out std_logic_vector(7 downto 0);
+        signal byte_in : in std_logic_vector(7 downto 0);
+        signal queue_empty : in boolean;
+        signal queue_pop : out boolean;
+        variable word_complete : out boolean) is
+    begin
+        word_complete := false;
+        if queue_wait_cycle then
+            queue_wait_cycle := false;
+        elsif not queue_empty then
+            byte_out := byte_in;
+            queue_pop <= true;
+            queue_wait_cycle := true;
+            word_complete := true;
+        end if;
+    end procedure;
+
+    procedure word_to_tx_queue(
+        variable queue_wait_cycle : inout boolean;
+        variable word_index_counter : inout natural range 0 to 3;
+        signal word : in std_logic_vector(31 downto 0);
+        signal byte : out std_logic_vector(7 downto 0);
+        signal queue_full : in boolean;
+        signal queue_push : out boolean;
+        variable word_complete : out boolean) is
+    begin
+        word_complete := false;
+        if queue_wait_cycle then
+            queue_wait_cycle := false;
+        elsif not queue_full then
+            byte <= word(word_index_counter*8 + 7 downto word_index_counter*8);
+            queue_push <= true;
+            queue_wait_cycle := true;
+            if word_index_counter = 3 then
+                word_complete := true;
+                word_index_counter := 0;
+            else
+                word_index_counter := word_index_counter + 1;
+                word_complete := false;
+            end if;
+        end if;
+    end procedure;
+
+    procedure byte_to_tx_queue(
+        variable queue_wait_cycle : inout boolean;
+        signal byte_out : out std_logic_vector(7 downto 0);
+        variable byte_in : in std_logic_vector(7 downto 0);
+        signal queue_full : in boolean;
+        signal queue_push : out boolean;
+    variable word_complete : out boolean) is
+    begin
+        word_complete := false;
+        if queue_wait_cycle then
+            queue_wait_cycle := false;
+        elsif not queue_full then
+            byte_out <= byte_in;
+            queue_push <= true;
+            queue_wait_cycle := true;
+            word_complete := true;
+        end if;
+    end procedure;
 begin
     fsm : process(clk)
         variable cur_state : state_type := state_wait_for_command;
@@ -88,6 +176,8 @@ begin
         variable word_index_counter : natural range 0 to 3 := 0;
         variable sequence_size : natural range 0 to 255;
         variable queue_wait_cycle : boolean := false;
+        variable word_complete : boolean := false;
+        variable byte_out : std_logic_vector(7 downto 0);
 
         variable bus_fault_occured : boolean := false;
         variable first_bus_fault : bus_pkg.bus_fault_type;
@@ -98,54 +188,48 @@ begin
             bus_do_read <= false;
             bus_do_write <= false;
             cur_state := next_state;
-
             case cur_state is
                 when state_wait_for_command =>
-                    if not rx_queue_empty then
-                        command := translateCommand(rx_queue_data_out);
-                        rx_queue_pop_data <= true;
+                    rx_queue_to_byte(queue_wait_cycle, byte_out, rx_queue_data_out, rx_queue_empty, rx_queue_pop_data,
+                                     word_complete);
+                    if word_complete then
+                        command := translateCommand(byte_out);
                         next_state := state_command_response;
                     end if;
                 when state_command_response =>
-                    if not tx_queue_full then
-                        tx_queue_push_data <= true;
+                    if command = no_command then
+                        byte_out := uart_bus_master_pkg.ERROR_UNKOWN_COMMAND;
+                    else
+                        byte_out := uart_bus_master_pkg.ERROR_NO_ERROR;
+                    end if;
+                    byte_to_tx_queue(queue_wait_cycle, tx_queue_data_in, byte_out, tx_queue_full, tx_queue_push_data,
+                                     word_complete);
+                    if word_complete then
                         if command = no_command then
-                            tx_queue_data_in <= uart_bus_master_pkg.ERROR_UNKOWN_COMMAND;
                             next_state := state_wait_for_command;
                         else
-                            tx_queue_data_in <= uart_bus_master_pkg.ERROR_NO_ERROR;
                             next_state := state_wait_for_address;
                         end if;
                     end if;
                 when state_wait_for_address =>
-                    if queue_wait_cycle then
-                        queue_wait_cycle := false;
-                    elsif not rx_queue_empty then
-                        address_to_bus(word_index_counter*8 + 7 downto word_index_counter*8) <= rx_queue_data_out;
-                        rx_queue_pop_data <= true;
-                        queue_wait_cycle := true;
+                    rx_queue_to_word(queue_wait_cycle, word_index_counter, address_to_bus, rx_queue_data_out,
+                                     rx_queue_empty, rx_queue_pop_data, word_complete);
 
-                        if (word_index_counter = 3) then
-                            word_index_counter := 0;
-                            sequence_size := 0;
-                            if command = command_read_word then
-                                next_state := state_read_word_from_bus;
-                            elsif command = command_write_word then
-                                next_state := state_read_word_from_uart;
-                            else
-                                next_state := state_wait_for_count;
-                            end if;
+                    if word_complete then
+                        sequence_size := 0;
+                        if command = command_read_word then
+                            next_state := state_read_word_from_bus;
+                        elsif command = command_write_word then
+                            next_state := state_read_word_from_uart;
                         else
-                            word_index_counter := word_index_counter + 1;
+                            next_state := state_wait_for_count;
                         end if;
                     end if;
                 when state_wait_for_count =>
-                    if queue_wait_cycle then
-                        queue_wait_cycle := false;
-                    elsif not rx_queue_empty then
-                        sequence_size := to_integer(unsigned(rx_queue_data_out));
-                        rx_queue_pop_data <= true;
-                        queue_wait_cycle := true;
+                    rx_queue_to_byte(queue_wait_cycle, byte_out, rx_queue_data_out, rx_queue_empty, rx_queue_pop_data,
+                                     word_complete);
+                    if word_complete then
+                        sequence_size := to_integer(unsigned(byte_out));
                         if command = command_write_word_sequence then
                             next_state := state_read_word_from_uart;
                         else
@@ -153,49 +237,32 @@ begin
                         end if;
                     end if;
                 when state_read_word_from_bus =>
-                    bus_do_read <= true;
                     if bus_finished then
-                        bus_do_read <= false;
                         if not bus_fault_occured and bus_fault then
                             first_bus_fault := bus_last_fault;
                             bus_fault_occured := true;
                         end if;
                         next_state := state_write_word_to_uart;
+                    else
+                        bus_do_read <= true;
                     end if;
                 when state_write_word_to_uart =>
-                    if queue_wait_cycle then
-                        queue_wait_cycle := false;
-                    elsif not tx_queue_full then
-                        tx_queue_data_in <= data_from_bus(word_index_counter*8 + 7 downto word_index_counter*8);
-                        tx_queue_push_data <= true;
-                        queue_wait_cycle := true;
-
-                        if word_index_counter = 3 then
-                            word_index_counter := 0;
-                            if sequence_size = 0 then
-                                next_state := state_finalize;
-                            else
-                                sequence_size := sequence_size - 1;
-                                address_to_bus <= std_logic_vector(unsigned(address_to_bus) + 4);
-                                next_state := state_read_word_from_bus;
-                            end if;
+                    word_to_tx_queue(queue_wait_cycle, word_index_counter, data_from_bus, tx_queue_data_in, tx_queue_full,
+                                     tx_queue_push_data, word_complete);
+                    if word_complete then
+                        if sequence_size = 0 then
+                            next_state := state_finalize;
                         else
-                            word_index_counter := word_index_counter + 1;
+                            sequence_size := sequence_size - 1;
+                            address_to_bus <= std_logic_vector(unsigned(address_to_bus) + 4);
+                            next_state := state_read_word_from_bus;
                         end if;
                     end if;
                 when state_read_word_from_uart =>
-                    if queue_wait_cycle then
-                        queue_wait_cycle := false;
-                    elsif not rx_queue_empty then
-                        data_to_bus(word_index_counter*8 + 7 downto word_index_counter*8) <= rx_queue_data_out;
-                        rx_queue_pop_data <= true;
-                        queue_wait_cycle := true;
-                        if (word_index_counter = 3) then
-                            word_index_counter := 0;
-                            next_state := state_write_word_to_bus;
-                        else
-                            word_index_counter := word_index_counter + 1;
-                        end if;
+                    rx_queue_to_word(queue_wait_cycle, word_index_counter, data_to_bus, rx_queue_data_out,
+                                     rx_queue_empty, rx_queue_pop_data, word_complete);
+                    if word_complete then
+                        next_state := state_write_word_to_bus;
                     end if;
                 when state_write_word_to_bus =>
                     bus_do_write <= true;
@@ -214,18 +281,16 @@ begin
                         end if;
                     end if;
                 when state_finalize =>
-                    if queue_wait_cycle then
-                        queue_wait_cycle := false;
-                    elsif not tx_queue_full then
-                        tx_queue_push_data <= true;
-                        queue_wait_cycle := true;
-                        if bus_fault_occured then
-                            tx_queue_data_in <= first_bus_fault & uart_bus_master_pkg.ERROR_BUS(3 downto 0);
-                        else
-                            tx_queue_data_in <= uart_bus_master_pkg.ERROR_NO_ERROR;
-                        end if;
-                        bus_fault_occured := false;
+                    if bus_fault_occured then
+                        byte_out := first_bus_fault & uart_bus_master_pkg.ERROR_BUS(3 downto 0);
+                    else
+                        byte_out := uart_bus_master_pkg.ERROR_NO_ERROR;
+                    end if;
+                    byte_to_tx_queue(queue_wait_cycle, tx_queue_data_in, byte_out, tx_queue_full, tx_queue_push_data,
+                                     word_complete);
+                    if word_complete then
                         next_state := state_wait_for_command;
+                        bus_fault_occured := false;
                     end if;
                 when others =>
                     next_state := cur_state;
